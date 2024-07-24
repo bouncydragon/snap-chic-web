39e7b07279041192e1f822c2e46d71386b1586bf
bouncydragon <duanecarycusi@gmail.com>
2024-07-24 23:28:52 +0800

    feat: a homepage first section design

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,71 @@
-import Image from "next/image";
+import { Check, Star } from "lucide-react";
+import MaxWidthWrapper from "@/components/MaxWidthWrapper";
+import Phone from "@/components/Phone";
 
 export default function Home() {
   return (
-    <div>Home</div>
+    <div className="bg-slate-50">
+      <section>
+        <MaxWidthWrapper className="pb-24 pt-10 lg:grid lg:grid-cols-3 sm:pb-32 lg:gap-x-0 xl:gap-x-8 lg:pt-24 xl:pt-32 lg:pb-52">
+          <div className="col-span-2 px-6 lg:px-0 lg:pt-4">
+            <div className="relative mx-auto text-center lg:text-left flex flex-col items-center lg:items-start">
+              <div className="absolute w-28 left-0 -top-20 hidden lg:block">
+                <img src="/snapchic-logo.png" className="w-full" />
+              </div>
+              <h1 className="relative w-fit tracking-tight text-balance mt-16 font-bold !leading-tight text-gray-900 text-5xl md:text-6xl lg:text-7xl">
+                Snap it with our <span className="bg-blue-700 px-2 text-white">Custom</span> Phone Case
+              </h1>
+              <p className="mt-8 text-lg lg:pr-10 max-w-prose text-center lg:text-left text-balance md:text-wrap">
+                Relive your favorite memories with <span className="font-semibold">every glance</span>. Our custom cases turn your phone into a beautiful keepsake of life&apos;s special moments.
+              </p>
+              <ul className="mt-8 space-y-2 text-left font-medium flex flex-col items-center sm:items-start">
+                <div className="space-y-2">
+                  <li className="flex gap-1.5 items-center text-left">
+                    <Check className="h-5 w-5 shrink-0 text-blue-700" />
+                    High-quality, durable material
+                  </li>
+                  <li className="flex gap-1.5 items-center text-left">
+                    <Check className="h-5 w-5 shrink-0 text-blue-700" />
+                    10 year print guarantee
+                  </li>
+                  <li className="flex gap-1.5 items-center text-left">
+                    <Check className="h-5 w-5 shrink-0 text-blue-700" />
+                    Modern iPhone and Samsung models supported
+                  </li>
+                </div>
+              </ul>
+              <div className="mt-12 flex flex-col sm:flex-row items-center sm:items-start gap-5">
+                <div className="flex -space-x-4">
+                  <img className="inline-block h-10 w-10 rounded-full ring-2 ring-slate-100 object-cover" src="/users/user-1.jpg" alt="user image" />
+                  <img className="inline-block h-10 w-10 rounded-full ring-2 ring-slate-100 object-cover" src="/users/user-2.jpg" alt="user image" />
+                  <img className="inline-block h-10 w-10 rounded-full ring-2 ring-slate-100 object-cover" src="/users/user-3.jpg" alt="user image" />
+                  <img className="inline-block h-10 w-10 rounded-full ring-2 ring-slate-100 object-cover" src="/users/user-4.jpg" alt="user image" />
+                  <img className="inline-block h-10 w-10 rounded-full ring-2 ring-slate-100 object-cover" src="/users/user-5.jpg" alt="user image" />
+                </div>
+                <div className="flex flex-col justify-between items-center sm:items-start">
+                  <div className="flex gap-0.5">
+                    <Star className="h-4 w-4 text-blue-700 fill-blue-700" />
+                    <Star className="h-4 w-4 text-blue-700 fill-blue-700" />
+                    <Star className="h-4 w-4 text-blue-700 fill-blue-700" />
+                    <Star className="h-4 w-4 text-blue-700 fill-blue-700" />
+                    <Star className="h-4 w-4 text-blue-700 fill-blue-700" />
+                  </div>
+                  <p>
+                    <span className="font-semibold">1.573</span> happy customers
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="col-span-full lg:col-span-1 w-full flex justify-center px-8 sm:px-16 md:px-0 mt-32 lg:mx-0 lg:mt-20 h-fit">
+            <div className="relative md:max-w-xl">
+              <img src="" className="absolute w-40 lg:w-52 left-56 -top-20 select-none hidden sm:block lg:hidden xl:block" />
+              <img src="/line.png" className="absolute w-20 -left-6 -bottom-6 select-none" />
+              <Phone className="w-64" imgSrc="" />
+            </div>
+          </div>
+        </MaxWidthWrapper>
+      </section>
+    </div>
   );
 }
